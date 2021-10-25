38a9c7b8273f9efea854295696020de1a40b2104
jhamPac <cloud@kevia.me>
2021-10-25 14:55:12 -0700

    chore: save point

--- src/Thirteen/Hangman.hs
+++ src/Thirteen/Hangman.hs
@@ -56,3 +56,27 @@ alreadyGuessed (Puzzle _ _ guessed) char = char `elem` guessed
 
 renderPuzzleChar :: Maybe Char -> Char
 renderPuzzleChar = fromMaybe '_'
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar cs) c = Puzzle word newFilledInSoFar (c : cs)
+                                                where
+                                                    zipper guessed wordChar guessChar =
+                                                        if wordChar == guessed
+                                                        then Just wordChar
+                                                        else guessChar
+
+                                                    newFilledInSoFar = zipWith (zipper c) word filledInSoFar
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+    putStrLn $ "Your guess was: " ++ [guess]
+    case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+        (_, True) -> do
+            putStrLn "You already guessed that character, pick something else!"
+            pure puzzle
+        (True, _) -> do
+            putStrLn "This character was in the word, filling in the word accordingly"
+            pure (fillInCharacter puzzle guess)
+        (False, _) -> do
+            putStrLn "This character wasn't in the word, try again."
+            pure (fillInCharacter puzzle guess)
