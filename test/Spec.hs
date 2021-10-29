a353577f252221f48a78abacde3eaada2a75b624
jhamPac <cloud@kevia.me>
2021-10-29 09:51:49 -0700

    feat: start module for morse

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,5 @@
 import           Fourteen.Addition (dividedBy)
-import           Test.Hspec        (describe, hspec, it, shouldBe)
 import           Test.QuickCheck   (Testable (property))
 
 main :: IO ()
-main = hspec $ do
-    describe "Addition" $ do
-        it "15 divided by 3 is 5" $ do
-            dividedBy 15 3 `shouldBe` (5, 0)
-        it "22 divided by 5 is 4 remainder 2" $ do
-            dividedBy 22 5 `shouldBe` (4, 2)
-        it "x + 1 is always greater than x" $ do
-            property $ \x -> x + 1 > (x :: Int)
+main = undefined
