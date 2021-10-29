55e911cc4818d112b6dd14f4a62d2eefc32857eb
jhamPac <cloud@kevia.me>
2021-10-29 10:20:17 -0700

    feat: converting to morse done

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,32 @@
 module Main where
 
-import           Data.Char        (toLower)
-import           System.IO        (BufferMode (NoBuffering), hSetBuffering,
-                                   stdout)
-import           Thirteen.Hangman (freshPuzzle, randomWord', runGame)
+import           Control.Monad      (forever, when)
+import           Data.List          (intercalate)
+import           Data.Traversable   (traverse)
+import           Fourteen.Morse     (morseToChar, stringToMorse)
+import           System.Environment (getArgs)
+import           System.Exit        (exitFailure, exitSuccess)
+import           System.IO          (hGetLine, isEOF, stdin)
 
 main :: IO ()
-main = do
-    hSetBuffering stdout NoBuffering
-    word <- randomWord'
-    let puzzle = freshPuzzle (fmap toLower word)
-    runGame puzzle
+
+convertToMorse :: IO ()
+convertToMorse = forever $ do
+    done <- isEOF
+    when done exitSuccess
+
+    line <- getLine
+    convertLine line
+    where
+        convertLine line = do
+            let morse = stringToMorse line
+            case morse of
+                (Just str) -> putStrLn (unwords str)
+                Nothing    -> do
+                                putStrLn $ "ERROR: " ++ line
+                                exitFailure
+
+
+
+main = print "Ye"
 
