1b9cf28e181f3e95dd85a7aceb95a2cedc5e29a1
jhamPac <cloud@kevia.me>
2021-10-28 19:21:22 -0700

    test: impot quick check and save point

--- src/Fourteen/Addition.hs
+++ src/Fourteen/Addition.hs
@@ -1,4 +1,8 @@
 module Fourteen.Addition where
 
-add :: IO ()
-add = putStrLn "Start"
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+    where
+        go n d count
+            | n < d = (count, n)
+            | otherwise = go (n - d) d (count + 1)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,13 @@
-import           Fourteen.Addition
-import           Test.Hspec
+import           Fourteen.Addition (dividedBy)
+import           Test.Hspec        (describe, hspec, it, shouldBe)
+import           Test.QuickCheck   (Testable (property))
 
 main :: IO ()
 main = hspec $ do
     describe "Addition" $ do
-        it "1 + 1 is greater than 1" $
-            (1 + 1) > 1 `shouldBe` True
+        it "15 divided by 3 is 5" $ do
+            dividedBy 15 3 `shouldBe` (5, 0)
+        it "22 divided by 5 is 4 remainder 2" $ do
+            dividedBy 22 5 `shouldBe` (4, 2)
+        it "x + 1 is always greater than x" $ do
+            property $ \x -> x + 1 > (x :: Int)
