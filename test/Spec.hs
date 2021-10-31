6294645c9b8e27080d951ab9d770b540dec167b5
jhamPac <cloud@kevia.me>
2021-10-31 14:50:46 -0700

    test: first real quickcheck test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,24 @@
-import           Fourteen.Addition (dividedBy)
-import           Test.QuickCheck   (Testable (property))
+import qualified Data.Map        as M
+import           Fourteen.Morse  (Morse, charToMorse, letterToMorse,
+                                  morseToChar)
+import           Test.QuickCheck (Gen, Property, elements, forAll, quickCheck)
+
+allowedChars :: [Char]
+allowedChars = M.keys letterToMorse
+
+allowedMorse :: [Morse]
+allowedMorse = M.elems letterToMorse
+
+charGen :: Gen Char
+charGen = elements allowedChars
+
+morseGen :: Gen Morse
+morseGen = elements allowedMorse
+
+prop_MorseAndBackAgain :: Property
+prop_MorseAndBackAgain =
+    forAll charGen
+        (\c -> (charToMorse c >>= morseToChar) == Just c)
 
 main :: IO ()
-main = undefined
+main = quickCheck prop_MorseAndBackAgain
