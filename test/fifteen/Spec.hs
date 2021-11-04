536fb0f98aaa56d8c4ced710c8e097ca262ed2f2
jhamPac <cloud@kevia.me>
2021-11-03 19:16:18 -0700

    test: passed yay!

--- test/fifteen/Spec.hs
+++ test/fifteen/Spec.hs
@@ -1,7 +1,7 @@
 import           Control.Monad
 import           Data.Monoid
-import           Fifteen.Excercises
-import           Test.QuickCheck
+import           Fifteen.Excercises (First')
+import           Test.QuickCheck    (quickCheck)
 
 monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
 monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
@@ -12,5 +12,8 @@ monoidLeftIdentity a = (mempty <> a) == a
 monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
 monoidRightIdentity a = (a <> mempty) == a
 
+main :: IO ()
 main = do
-    quickCheck (monoidAssoc :: First' String -> First' String => First String -> Bool)
+    quickCheck (monoidAssoc :: First' String -> First' String -> First' String -> Bool)
+    quickCheck (monoidLeftIdentity :: First' (Sum Int) -> Bool)
+    quickCheck (monoidRightIdentity :: First' (Sum Int) -> Bool)
